ea0fed95fffa1578cf20bf295a2db7e7e0223733
rohanrvpr hue <rohanrvpr@gmail.com>
2026-07-13 09:02:56 +0530

    Add activation tracker module skeleton

---
+++ src/activation_tracker.py
@@ -0,0 +1,28 @@
+"""
+NeuroFence
+
+Module:
+Activation Tracker
+
+Author:
+Rohan
+
+Purpose:
+This module will monitor hidden layer activations
+of the LLM during inference.
+
+Current Status:
+Week 1 - Project Setup
+(No implementation yet)
+"""
+class ActivationTracker:
+    """
+    Activation Tracker Class
+
+    This class will later be responsible for:
+    - Tracking hidden layer activations
+    - Saving activation values
+    - Helping detect suspicious behavior
+    """
+
+    pass
