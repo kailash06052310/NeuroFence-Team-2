83a5b64c3957cd16eb7cc96daa22a17736db903e
rohanrvpr hue <rohanrvpr@gmail.com>
2026-07-19 09:52:29 +0530

    Test ActivationTracker module

---
+++ src/main.py
@@ -0,0 +1,18 @@
+from activation_tracker import ActivationTracker
+
+# Create object
+tracker = ActivationTracker()
+
+# Store activation values
+tracker.save_activation("Layer_1", [0.12, 0.45, 0.78])
+tracker.save_activation("Layer_2", [0.31, 0.65, 0.92])
+
+# Print one layer
+print("Layer_1:")
+print(tracker.get_activation("Layer_1"))
+
+print()
+
+# Print all layers
+print("All Activations:")
+print(tracker.get_all_activations())
