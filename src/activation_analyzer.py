9718d3884e661b169fc262d8b17347e486d5311a
rohanrvpr hue <rohanrvpr@gmail.com>
2026-07-31 19:36:49 +0530

    Add Activation Analyzer module structure

---
+++ src/activation_analyzer.py
@@ -0,0 +1,21 @@
+"""
+NeuroFence Project
+
+Module: Activation Analyzer
+
+Author: Rohan
+
+Purpose:
+Analyze hidden layer activations collected by the Activation Tracker.
+This module calculates statistical information from activations.
+"""
+class ActivationAnalyzer:
+    """
+    Analyze activations captured from transformer layers.
+    """
+
+    def __init__(self):
+        """
+        Initialize analyzer.
+        """
+        pass
